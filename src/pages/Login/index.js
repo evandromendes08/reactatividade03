d8059325119f6b016bd4582bea4b341ca0e8bf55
Evandro Mendes <evandromende@gmail.com>
2024-06-11 20:03:55 -0300

    Feat: AuthContext

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/logo.png'
+import { useNavigate } from 'react-router-dom';
 
-const index = () => {
+const Login = () => {
+  const [inputValues, setInputValues] = useState({
+    email:'',
+    senha:''
+  })
+
+  const navigate = useNavigate(); 
+
+  const handleChangeValues = (evento) =>{
+    setInputValues({
+      ...inputValues,
+      [evento.target.name]:evento.target.value
+    })
+    console.log(inputValues);
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`usuário ${inputValues.email} logado com sucesso.`)
+    navigate('/')
+  }
   return(
     <main className='h-screen w-full'>
       <div className='flex flex-col pt-20 items-center h-screen'>
         <img className='w-52' src={logo} alt='logotipo food app'/>
-        <form action="" className='bg-white w-96 mt-6 p-4 rounded-lg shadon-lg'>
+        <form onSubmit={handleSubmit} className='bg-gray-300 w-96 mt-6 p-4 rounded-lg shadon-lg'>
           <div className='flex flex-col space-y-6'>
             <input type="mail" placeholder="Digite o seu email:" name="email"
             className="w-full px-4 py-3 rounded-lg ring-red-200 border-gray-300 focus:ring-4 focus:outline-none transition duration-300 focus:shadow-xl"
+            onChange={handleChangeValues}
             />
             <input type="password" placeholder="Digite sua senha:" name="senha"
-            className="w-full px-4 py-3 rounded-lg ring-red-200 border-gray-300 focus:ring-4 focus:outline-none transition duration-300 focus:shadow-xl"
+            className="w-full px-4 py-3 rounded-lg ring-red-200 border-gray-700 focus:ring-4 focus:outline-none transition duration-300 focus:shadow-xl"
+            onChange={handleChangeValues}
             />
           </div>
-          <button className="w-full py-3 bg-primary text-white focus:outline-none focus:ring-4 mt-6 rounded-lg transition duration-300">
+          <button type="submit" className="w-full py-3 bg-primary text-white focus:outline-none focus:ring-4 mt-6 rounded-lg transition duration-300">
             Entrar
           </button>
           <p className="text-base text-primary text-center my-6 hover:underline cursor-pointer"> Precisa de uma conta?</p>
@@ -25,4 +47,4 @@ const index = () => {
   )
 }
 
-export default index;
+export default Login;
